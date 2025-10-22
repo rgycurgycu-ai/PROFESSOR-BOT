50d22877bfd281ac1b9c8e013dd33eaf5df0ac5e
rgycurgycu-ai <rgycurgycu@gmail.com>
2025-10-22 05:54:03 -0700

    Update info.py

--- info.py
+++ info.py
@@ -14,7 +14,7 @@ from os import environ
 # PyroClient Setup
 API_ID = int(environ.get('API_ID', '21643763'))
 API_HASH = environ.get('API_HASH', '9ab0b0e819951db76417c7e4962ccdf5')
-BOT_TOKEN = environ.get('BOT_TOKEN', '7262086950:AAFFjZkbFtdAnizcDgFUmJuY8uAQhfz8CPk')
+BOT_TOKEN = environ.get('BOT_TOKEN', '7262086950:AAF8LfKixReHtxWqIsfbas65saWL0F5ypvY')
 
 
 # Bot settings
@@ -26,7 +26,7 @@ UPTIME = time.time()
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5095951697').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1003186766542').split()]
-auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
+auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '-1002767321414').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_channel = environ.get('AUTH_CHANNEL')
 auth_grp = environ.get('AUTH_GROUP')
